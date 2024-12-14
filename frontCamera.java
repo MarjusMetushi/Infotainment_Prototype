8ba2304151016311934aceea115c4341e05ccb91
MarjusMetushi <marjusmetushi1@gmail.com>
2024-12-14 18:42:50 +0100

    planned the front camera phase

---
+++ frontCamera.java
@@ -0,0 +1,13 @@
+public class frontCamera {
+    /*
+     * Requirements:
+     *  Create the interface for the front camera in java 
+     *  Create a method to display the front camera in the GUI c++ to access the driver and camera but java to display it as a thread
+     *  Create a method to take a picture with the front camera and save it somewhere c++
+     *  Create a method to increase/decrease the brightness of the front camera java 
+     *  Create a method to add a timer to the front camera java and call the c++ executable
+     */
+    frontCamera() {
+
+    }
+}
