fe43ed82fcb7dfad8e9a222e478217ebb96670e4
MarjusMetushi <marjusmetushi1@gmail.com>
2025-01-20 23:52:42 +0100

    Researched and built a step by step plan for building the map

---
+++ map.java
@@ -0,0 +1,14 @@
+public class map {
+    /*
+     * Map application step by step guide
+     * 1. Create the frame and the main interface
+     * 2. Download the jar file of OSMDroid and move it here
+     * 3. Read the documentation of OSMDroid
+     * 4. Create the map
+     * 5. Create the marker
+     * 6. Create the overlay
+     * 7. Create the zoom control
+     * 8. Create the scale control
+     */
+    map(){}
+}
