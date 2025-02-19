fddcbc1aa38b9d6af36a164440e36f3416a98312
Marjus <marjusmetushi1@gmail.com>
2025-02-19 17:57:13 +0100

    Designed new plan and wrote the script. Testing is required

--- iphoneconnection.py
+++ iphoneconnection.py
@@ -1,6 +1,17 @@
-# Use a webRTC server to stream the screen
-# Install pip install aiortc flask opencv-python numpy
-# create the server
-# start the server
-# phone -> web -> localhost 
-# create a client receiver
+# get a hdmi capture card
+# get a hdmi
+# connect to the hdmi
+# run this script
+
+import cv2
+
+cv = cv2.VideoCapture(0)
+
+while True:
+    ret, frame = cv.read()
+    cv2.imshow('frame', frame)
+    if cv2.waitKey(1) & 0xFF == ord('q'):
+        break
+
+cv.release()
+cv2.destroyAllWindows()
